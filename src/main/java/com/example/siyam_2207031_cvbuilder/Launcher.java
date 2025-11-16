be68e5bf59023399fc929a561a9175a6b6446f84
Sadikul-Islam-Siyam <siyam2207031@stud.kuet.ac.bd>
2025-11-16 22:45:11 +0600

    first commit

---
+++ src/main/java/com/example/siyam_2207031_cvbuilder/Launcher.java
@@ -0,0 +1,9 @@
+package com.example.siyam_2207031_cvbuilder;
+
+import javafx.application.Application;
+
+public class Launcher {
+    public static void main(String[] args) {
+        Application.launch(HelloApplication.class, args);
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module com.example.siyam_2207031_cvbuilder {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens com.example.siyam_2207031_cvbuilder to javafx.fxml;
+    exports com.example.siyam_2207031_cvbuilder;
+}
