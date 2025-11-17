2ec8ec6b9dd43409a6ff978fb20ea77795018de5
Sadikul-Islam-Siyam <siyam2207031@stud.kuet.ac.bd>
2025-11-17 09:20:00 +0600

    Home screen layout and controller

--- src/main/java/com/example/siyam_2207031_cvbuilder/controller/HomeController.java
+++ src/main/java/com/example/siyam_2207031_cvbuilder/controller/HomeController.java
@@ -1,4 +1,24 @@
-package com.example.siyam_2207031_cvbuilder.controller;
+package com.cvbuilder.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+
+ //Controller for the Home Screen
 
 public class HomeController {
+
+    @FXML
+    private Button createNewCVButton;
+
+    private SceneManager sceneManager;
+
+    @FXML
+    public void initialize() {
+        createNewCVButton.setOnAction(event -> sceneManager.showCVFormScene());
+    }
+
+    public void setSceneManager(SceneManager sceneManager) {
+        this.sceneManager = sceneManager;
+    }
 }
